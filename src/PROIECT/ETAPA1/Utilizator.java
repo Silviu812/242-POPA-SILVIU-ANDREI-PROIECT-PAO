08ed066b9278eb3e15b0784e2e3a82de21bf663f
Silviu <silviuandrei1056@gmail.com>
2024-04-06 15:54:06 +0300

    utilizator

---
+++ src/PROIECT/ETAPA1/Utilizator.java
@@ -0,0 +1,17 @@
+package PROIECT.ETAPA1;
+
+public class Utilizator {
+    private String nume;
+    private String email;
+    private String parola;
+
+    public Utilizator(String nume, String email, String parola) {
+        this.nume = nume;
+        this.email = email;
+        this.parola = parola;
+    }
+
+    // GET
+
+    // SET
+}
